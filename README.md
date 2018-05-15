94badece7920e9897eb77515b6671c88c0ac288e
Zhang Qiang <zhangqiang@meizu.com>
2018-05-16 00:37:46 +0800

    module net: detach

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ An agent running on hosts to control virtual machines remotely via libvirt API.
 Currently supports:
 - volume create, attach, detach, delete
 - power operations including suspend, resume
+- net interface detach
 
 A few things to notice before using:
 - It uses binary protocol for rapid prototyping but might not be very friendly to the implementation of the server
--- modules/net/detach.go
+++ modules/net/detach.go
@@ -3,10 +3,13 @@ package net
 import (
 	"bytes"
 	"encoding/binary"
+	"errors"
 	"fmt"
+	"github.com/beevik/etree"
 	"github.com/dotSlashLu/agent-neo/lib"
 	llib "github.com/dotSlashLu/agent-neo/lib/libvirt"
 	"github.com/libvirt/libvirt-go"
+	"log"
 	"strings"
 )
 
@@ -22,11 +25,6 @@ const (
 // 	MAC  [17]byte
 // 	VLAN int16
 // 	Type netType
-//
-// Right now the device's bridge is hardcoded as ovsbr0 / ovsbr1 according to
-// the netType.
-// Better find the device by parsing the VM's definition XML to find the
-// device by its MAC.
 func (m *Module) detach(recv []byte) ([]byte, error) {
 	type paramsProto struct {
 		UUID llib.UUID
@@ -39,42 +37,48 @@ func (m *Module) detach(recv []byte) ([]byte, error) {
 	uuid := lib.TrimBuf(params.UUID[:])
 	mac := lib.TrimBuf(params.MAC[:])
 
-	vlanXML := ""
-	if params.VLAN != 0 {
-		vlanXML = fmt.Sprintf(`
-			<vlan>
-				<tag id="%s" />
-			</vlan>
-		`, params.VLAN)
-	}
-	bridge := "ovsbr0"
-	if params.Type == netTypeInter {
-		bridge = "ovsbr1"
-	}
-	dev := strings.Replace(string(mac), ":", "", -1)
-	ifXML := fmt.Sprintf(`
-		<interface type='bridge'>
-			<mac address='%s'/>
-			<source bridge='%s'/>
-			<virtualport type='openvswitch'>
-			</virtualport>
-			<target dev='v%s'/>
-			<model type='virtio'/>
-			%s
-		</interface>
-	`, string(mac), bridge, dev, vlanXML)
 	conn, err := llib.Connect()
 	if err != nil {
 		return lib.RespError(err)
 	}
+	defer func() {
+		conn.Close()
+	}()
 	dom, err := conn.LookupDomainByUUIDString(string(uuid))
 	if err != nil {
 		return lib.RespError(err)
 	}
+	domXML, err := dom.GetXMLDesc(libvirt.DOMAIN_XML_SECURE)
+	if err != nil {
+		return lib.RespError(err)
+	}
+	doc := etree.NewDocument()
+	if err = doc.ReadFromString(domXML); err != nil {
+		return lib.RespError(err)
+	}
+	domain := doc.SelectElement("domain")
+	devices := domain.SelectElement("devices")
+	ifaces := devices.SelectElements("interface")
+	var iface *etree.Element
+	for _, iface = range ifaces {
+		macElem := iface.SelectElement("mac")
+		if macElem.SelectAttrValue("address", "") == string(mac) {
+			break
+		}
+	}
+	if iface == nil {
+		return lib.RespError(errors.New("can't find iface"))
+	}
+	newDoc := etree.NewDocument()
+	newDoc.SetRoot(iface)
+	ifaceXML, _ := newDoc.WriteToString()
+	// ifaceXML := iface.Text()
+	log.Println("ifacexml", ifaceXML)
 	flags := libvirt.DOMAIN_DEVICE_MODIFY_CONFIG |
 		libvirt.DOMAIN_DEVICE_MODIFY_LIVE
-	if err = dom.DetachDeviceFlags(ifXML, flags); err != nil {
+	if err = dom.DetachDeviceFlags(ifaceXML, flags); err != nil {
 		return lib.RespError(err)
 	}
 	return lib.RespOk("")
 }
+
